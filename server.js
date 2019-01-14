a0b033d6094872de7ed59cc98948bfeb55062780
tgd <zjrzpp0414@163.com>
2019-01-14 11:28:56 +0800

    import data in database_zhihu

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 let app = express()
-app.listen(9000)
+app.listen(3000)
 
+app.use(bodyParser.urlencoded({extended:false}))
 // 用户以get方式在地址/a上要东西
 app.get('/a', (req, res, next) => {
     // res.send({a:2,b:3})
@@ -13,9 +15,12 @@ app.get('/a', (req, res, next) => {
    console.log('bbbbb')
 })
 
-// app.post('/upload', () => {
-//     console.log('请求upload')
-// })
+app.post('/upload', (req,res) => {
+    console.log(req.body)
+    res.write('{code:0,msg:"success"}');
+    // res.header(200)
+    res.end()
+})
 
 
 app.use(express.static('www/'))
